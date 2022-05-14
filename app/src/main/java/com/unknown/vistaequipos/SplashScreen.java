f3fdc6713fd9dfa06241f0cc13a912f99e9d986a
Sergio <sergio.gabriel.oficial@gmail.com>
2022-05-14 00:29:10 -0400

    Primera Version

---
+++ app/src/main/java/com/unknown/vistaequipos/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.unknown.vistaequipos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread background = new Thread(){
+            public void run(){
+                try {
+                    sleep(2500);
+                    Intent i = new Intent(getBaseContext(),MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }catch (Exception e){
+                }
+            }
+        };
+        background.start();
+    }
+}
